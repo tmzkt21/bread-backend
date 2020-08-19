6b75f37534dc5b5425b388f0ccb54e22367a2148
이승규 <62980952+tmzkt21@users.noreply.github.com>
2020-08-20 00:10:42 +0900

    더미데이터 생성 완료  user
    더미데이터 생성 완료  user

---
+++ src/main/java/com/bread/web/dummy/DummyUserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.bread.web.dummy;
+
+import com.bread.web.user.User;
+import com.bread.web.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+interface DummyUserService {
+    List<User> createDummyUser();
+}
+
+@Service
+public class DummyUserServiceImpl implements DummyUserService{
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Override
+    public List<User> createDummyUser() {
+        List<User> userList = new ArrayList<>();
+        for(int i = 0; i < 10000; i++) {
+            User user = new User();
+            user.setUserId(DummyUser.generateRandomId()+DummyUser.generateRandomNo2());
+            user.setPassword(DummyUser.generateRandomPw()+DummyUser.generateRandomPwNum());
+            user.setName(DummyUser.generateRandomName());
+            user.setEmail(DummyUser.generateRandomEmailId()+"@"+DummyUser.generateRandomEmail()+
+                    DummyUser.generateRandomEmailEnd());
+            user.setPhone(DummyUser.generateRandomPhone()+DummyUser.generateRandomPhoneNo()+DummyUser.generateRandomPhoneNo());
+            userList.add(user);
+        }
+        return userRepository.saveAll(userList);
+    }
+}
